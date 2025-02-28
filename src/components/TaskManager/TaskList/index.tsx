612d29758b011958eebc8470ca78957961b10e0b
Leandro Cotti <52415035+Lefcott@users.noreply.github.com>
2025-02-28 04:34:48 -0300

    Code refactoring and highlight selected tab (#5)
    * feat: separate task manager into components
    
    * feat: restructure component hierarchy
    
    * fix: uninstall zustand (will be part of future PR)
    
    * fix: revert package json changes

---
+++ src/components/TaskManager/TaskList/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Task } from "../../../types/task";
+import TaskItem from "./TaskItem";
+
+type Props = {
+  tasks: Task[];
+  onDelete: (id: number) => void;
+  onToggle: (id: number) => void;
+};
+
+const TaskList = ({ tasks, onDelete, onToggle }: Props) => (
+  <ul>
+    {tasks.map((task) => (
+      <TaskItem
+        key={task.id}
+        task={task}
+        onDelete={onDelete}
+        onToggle={onToggle}
+      />
+    ))}
+  </ul>
+);
+
+export default TaskList;
